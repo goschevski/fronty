dd1ce5dca3a5122d6757c0ff4a969fb4f7b7fff6
salencebg <gosevskibg@gmail.com>
2012-12-10 20:33:33 +0100

    Implemented iffe

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,70 +1,72 @@
-var App = (function (me, $) {
+;(function () {
+    window.App = (function (me, $) {
 
-    // Default config
-    var _config = {
-        holder: 'body'
-    },
+        // Default config
+        var _config = {
+            holder: 'body'
+        },
 
-    /* Checks if submodule exist and if route is good call method */
-    _executeSubmodule = function (obj, parts) {
-        var i = 0,
-            partsLen = parts.length,
-            part;
+        /* Checks if submodule exist and if route is good call method */
+        _executeSubmodule = function (obj, parts) {
+            var i = 0,
+                partsLen = parts.length,
+                part;
 
-        for (; i < partsLen; i++) {
-            part = parts[i];
+            for (; i < partsLen; i++) {
+                part = parts[i];
 
-            if (obj !== null && typeof obj === "object" && part in obj) {
-                obj = obj[part];
-            } else {
-                return false;
+                if (obj !== null && typeof obj === "object" && part in obj) {
+                    obj = obj[part];
+                } else {
+                    return false;
+                }
             }
-        }
 
-        return obj && obj.call(me);
-    },
+            return obj && obj.call(me);
+        },
 
-    /* Takes module names from holder and run it */
-    _loadSubmodules = function(options) {
-        // Extend config with options
-        $.extend(_config, options);
+        /* Takes module names from holder and run it */
+        _loadSubmodules = function(options) {
+            // Extend config with options
+            $.extend(_config, options);
 
-        var holder = $(_config.holder).data('modules'),
-            modules = holder ? holder.split(" ") : [],
-            modulesLength = modules.length,
-            submodule;
+            var holder = $(_config.holder).data('modules'),
+                modules = holder ? holder.split(" ") : [],
+                modulesLength = modules.length,
+                submodule;
 
-        while ( modulesLength && modulesLength-- ) {
-            submodule = modules[modulesLength].split("-");
-            _executeSubmodule( me, submodule );
-        }
-    },
+            while ( modulesLength && modulesLength-- ) {
+                submodule = modules[modulesLength].split("-");
+                _executeSubmodule( me, submodule );
+            }
+        },
 
-    /* Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking). */
-    _removeAddressBar = function () {
-        if (Modernizr.touch && !window.location.hash) {
-            $(window).load(function () {
-                setTimeout(function () {
-                    window.scrollTo(0, 1);
-                }, 0);
-            });
-        }
-    };
-
-    me.init = function (options) {
-        return {
-            submodules: _loadSubmodules(options),
-            addressbar: _removeAddressBar()
+        /* Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking). */
+        _removeAddressBar = function () {
+            if (Modernizr.touch && !window.location.hash) {
+                $(window).load(function () {
+                    setTimeout(function () {
+                        window.scrollTo(0, 1);
+                    }, 0);
+                });
+            }
         };
-    };
 
-    return me;
+        me.init = function (options) {
+            return {
+                submodules: _loadSubmodules(options),
+                addressbar: _removeAddressBar()
+            };
+        };
 
-})(App || {}, jQuery);
+        return me;
+
+    })(window.App || {}, jQuery);
+})();
 /*--------------------------------------
 * MAIN JS FILE
 ---------------------------------------*/
 
-$(function () {
+(function ($) {
 	App.init();
-});
+})(jQuery);
