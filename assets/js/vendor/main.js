862f85912dda5f4b5115065b386f22763eb6af6f
salencebg <gosevskibg@gmail.com>
2013-01-28 16:12:42 +0100

    Added placeholder and remove addressbar backups

--- assets/js/vendor/main.js
+++ assets/js/vendor/main.js
@@ -4,4 +4,37 @@
 
 $(function () {
 	App = new App.Views.App();
+
+	/* Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking). */
+	if (Modernizr.touch && !window.location.hash) {
+		$(window).load(function () {
+			setTimeout(function () {
+				window.scrollTo(0, 1);
+			}, 0);
+		});
+	}
+
+	/* Placeholder backup */
+	if(!Modernizr.input.placeholder){
+		('[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() === input.attr('placeholder')) {
+				input.val('');
+			}
+		}).blur(function() {
+			var input = $(this);
+			if (input.val() === '' || input.val() === input.attr('placeholder')) {
+				input.val(input.attr('placeholder'));
+			}}).blur();
+               
+
+		$('[placeholder]').parents('form').submit(function() {
+			$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() === input.attr('placeholder')) {
+					input.val('');
+				}
+			});
+		});
+	}
 });
