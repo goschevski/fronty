8e2634b9731a61d2da09def6c1f221b0262374a8
salencebg <gosevskibg@gmail.com>
2014-07-27 22:53:38 +0200

    Added autoprefixer, imagemin and compass through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    compass = require('gulp-compass'),
     minifycss = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
     browserify = require('gulp-browserify'),
     uglify = require('gulp-uglify');
 
 
 gulp.task('css', function () {
-    return gulp.src('assets/css/style.css')
+    gulp.src('sass/style.scss')
+        .pipe(compass({ config_file: 'config.rb', css: 'assets/css', sass: 'sass' }))
+        .pipe(autoprefixer('last 2 version', '> 1%', 'ie 8'))
         .pipe(minifycss())
         .pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('images', function () {
+    gulp.src('assets/img/**/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('assets/img'));
 });
 
 gulp.task('js', function () {
@@ -18,8 +29,9 @@ gulp.task('js', function () {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('assets/css/style.css', ['css']);
+    gulp.watch('sass/**/*.scss', ['css']);
     gulp.watch(['assets/js/**/*.js', '!assets/js/build/*.js'], ['js']);
 });
 
 gulp.task('default', ['css', 'js']);
+gulp.task('build', ['css', 'js', 'images']);
